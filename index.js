49bb1671617e3318bc0a3ab6ab7e3ae7a8d2c710
Villapalos <avillapalos@gmail.com>
2023-05-17 18:00:07 +0200

    Stripe configuration

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+app.use(cors());
 
 app.use(express.static("public"));
 app.use(express.json());
 
 app.post("/create-payment-intent", async (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-
     const { amount } = req.body;
 
     // Create a PaymentIntent with the order amount and currency
