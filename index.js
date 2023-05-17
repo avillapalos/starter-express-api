944c4a9c785da63a618580d26e5a9e74ddf51474
Villapalos <avillapalos@gmail.com>
2023-05-17 16:49:40 +0200

    Stripe configuration

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/create-payment-intent", async (req, res) => {
+    const { amount } = req.body;
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: "eur",
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+app.listen(4242);
