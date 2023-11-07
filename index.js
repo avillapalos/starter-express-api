3e54798a44447e54c55808ff6dcac9ebc406c878
Villapalos <avillapalos@gmail.com>
2023-11-07 17:10:57 +0100

    Sequra integration

--- index.js
+++ index.js
@@ -116,6 +116,12 @@ app.post("/create-payment-intent", async (req, res) => {
     });
 });
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
+
 app.post("/sequra-form", async (req, res) => {
     let data = {};
     try {
